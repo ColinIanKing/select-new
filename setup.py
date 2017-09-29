27961be4f2d0ea441c94f90aeb46bf2865ae1a82
Lucas Serrano <Lucas.Serrano@lip6.fr>
2017-09-29 11:22:03 +0200

    Enhanced configuration script

--- setup.py
+++ setup.py
@@ -6,19 +6,30 @@ from os import path
 from string import Template
 
 PREQUEL_BIN_NAME = "prequel.opt"
+SEARCH_ORDER = ['/bin/', '/usr/bin/', '/usr/local/', path.expanduser('~'), '/']
 
 def locate(name):
-    result = subprocess.run(["locate", "--limit=1", name],
-                            stdout=subprocess.PIPE, check=1)
-    result_path = result.stdout.splitlines()[0]
+    """Tries to find `name` using locate command"""
+    result_path = ""
+    for directory in SEARCH_ORDER:
+        research_exp = directory + '*' + name
+        try:
+            result = subprocess.run(["locate", "--limit=1", research_exp],
+                                    stdout=subprocess.PIPE, check=1)
+            result_path = result.stdout.splitlines()[0]
+            break
+        except:
+            continue
     return result_path.decode()
 
 
 def get_default_cores():
+    """Returns the number of logical cores available"""
     return cpu_count()
 
 
 def get_default_prequel_bin():
+    """Returns a default path for the prequel native binary"""
     prequel_bin = which(PREQUEL_BIN_NAME)
 
     if prequel_bin:
@@ -31,6 +42,7 @@ def get_default_prequel_bin():
 
 
 def get_default_indices_dir():
+    """Returns a default path for the directory containing prequel indices"""
     try:
         index_path = locate("linux_idutils_minus_word_U0.index")
         return path.dirname(index_path)
@@ -39,6 +51,7 @@ def get_default_indices_dir():
 
 
 def ask_field(field, default=""):
+    """Asks questions to the user in a `adduser` command fashion"""
     answer = ""
     while not answer:
         answer = input("%s [%s]: " % (field, default))
@@ -50,7 +63,7 @@ def ask_field(field, default=""):
 
 if __name__=='__main__':
     print("Enter the new value, or press ENTER for the default")
-    nb_cores = int(ask_field("Number of threads:",
+    nb_cores = int(ask_field("Number of threads",
                              default=get_default_cores())
     )
     prequel_bin = path.abspath(ask_field("Path to %s binary" % PREQUEL_BIN_NAME,
